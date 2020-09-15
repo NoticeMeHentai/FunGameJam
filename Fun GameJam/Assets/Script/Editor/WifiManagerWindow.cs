f086f5443c097d1a05f7cb462873a30fdef0d42c
NoticeMeHentai <43136048+NoticeMeHentai@users.noreply.github.com>
2020-09-15 19:23:49 +0200

    Wifi points update
    Update on wifi points relay system
    Started working on a editor window to quickly manage them on the map
    Added a tag and a physics layer for them

---
+++ Fun GameJam/Assets/Script/Editor/WifiManagerWindow.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class WifiManagerWindow : EditorWindow
+{
+    [MenuItem("Window/Wifi Manager", priority =0)]
+    public static void ShowWindow()
+    {
+        GetWindow<WifiManagerWindow>("Wifi Manager");
+    }
+
+    private static GUIStyle _ToggleButtonStyleNormal = null;
+    private static GUIStyle _ToggleButtonStyleToggled = null;
+
+    private enum WifiModificationStatus
+    {
+        None,
+        Placing,
+        Removing,
+        Unifying,
+    }
+    private static WifiModificationStatus _CurrentModificationStatus;
+    private static WifiPoint _CurrentSelectedPoint;
+
+    private GameObject _WifiPointPrefab;
+
+    private bool mHasDoneAction = false;
+    private bool _IsActive = false;
+
+    private void OnGUI()
+    {
+        //Initialize
+        if (_ToggleButtonStyleNormal == null)
+        {
+            _ToggleButtonStyleNormal = "Button";
+            _ToggleButtonStyleToggled = new GUIStyle(_ToggleButtonStyleNormal);
+            _ToggleButtonStyleToggled.normal.background = _ToggleButtonStyleToggled.active.background;
+        }
+
+        _WifiPointPrefab = EditorGUILayout.ObjectField(_WifiPointPrefab, typeof(GameObject),true) as GameObject;
+        _IsActive = GUILayout.Toggle(_IsActive, "IsActive");
+        //if (GUILayout.Button("Active", _IsActive ? _ToggleButtonStyleToggled : _ToggleButtonStyleNormal)) _IsActive = !_IsActive;
+        EditorGUILayout.TextArea("Shift + Left Click = Add \n Shift + Right Click = Relay \n Ctrl + Left Click = Delete");
+        //Window
+        //GUILayout.BeginHorizontal();
+        //if (GUILayout.Button("None", _CurrentModificationStatus== WifiModificationStatus.None ? _ToggleButtonStyleToggled : _ToggleButtonStyleNormal))
+        //{
+        //    _CurrentModificationStatus = WifiModificationStatus.None;
+        //}
+
+        //if (GUILayout.Button("Placing", _CurrentModificationStatus == WifiModificationStatus.Placing ? _ToggleButtonStyleToggled : _ToggleButtonStyleNormal))
+        //{
+        //    _CurrentModificationStatus = WifiModificationStatus.Placing;
+        //}
+        //GUILayout.EndHorizontal();
+
+        //GUILayout.BeginHorizontal();
+        //if (GUILayout.Button("Removing", _CurrentModificationStatus == WifiModificationStatus.Removing ? _ToggleButtonStyleToggled : _ToggleButtonStyleNormal))
+        //{
+        //    _CurrentModificationStatus = WifiModificationStatus.None;
+        //}
+
+        //if (GUILayout.Button("Unifying", _CurrentModificationStatus == WifiModificationStatus.Unifying ? _ToggleButtonStyleToggled : _ToggleButtonStyleNormal))
+        //{
+        //    _CurrentModificationStatus = WifiModificationStatus.None;
+        //}
+        //GUILayout.EndHorizontal();
+
+        if (_IsActive && !mHasDoneAction)
+        {
+            if (Event.current.shift && Event.current.button == 0) AddWifiPoint();
+            else if (Event.current.control && Event.current.button == 1) RemoveWifiPoint();
+            else if (Event.current.shift && Event.current.button == 1) RelayWifiPoints();
+        }
+
+    }
+
+    private void Update()
+    {
+        mHasDoneAction = false;
+    }
+
+    private void AddWifiPoint()
+    {
+        mHasDoneAction = true;
+        Debug.Log("Adding Wifi point");
+        if (_WifiPointPrefab!=null)
+        Instantiate(_WifiPointPrefab);
+    }
+
+    private void RemoveWifiPoint()
+    {
+        mHasDoneAction = true;
+        Debug.Log("Removing Wifi point");
+    }
+
+    private bool mTest = false;
+    private void RelayWifiPoints()
+    {
+        mTest = !mTest;
+        mHasDoneAction = true;
+        Debug.Log("Relaying Wifi point "+mTest.ToString());
+    }
+}
--- Fun GameJam/Assets/Script/WifiPoint.cs
+++ Fun GameJam/Assets/Script/WifiPoint.cs
@@ -1,8 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(SphereCollider))]
 public class WifiPoint : MonoBehaviour
 {
     public List<WifiPoint> mWifiPoints = new List<WifiPoint>();
+    //Editor utilites
+    public float mRadius = 3f;
+    [HideInInspector]public Color mColorToGizmos = Color.blue;
+
+
+
+    private SphereCollider mSphereCollider;
+    private SphereCollider _Collider { get { if (mSphereCollider == null) mSphereCollider = GetComponentInChildren<SphereCollider>(); return mSphereCollider; } }
+
+
+    /// <summary>
+    /// Returns a random point among the next points available
+    /// </summary>
+    /// <returns></returns>
+    public WifiPoint GetRandomNextWifiPoint()
+    {
+        if (mWifiPoints.Count > 0)
+            return mWifiPoints[Random.Range(0, mWifiPoints.Count)];
+        return this;
+    }
+    
+
+    public void RemoveWifiPoint(WifiPoint wifiPoint)
+    {
+        if (mWifiPoints.Contains(wifiPoint))
+            mWifiPoints.Remove(wifiPoint);
+    }
+
+    private void OnDrawGizmos()
+    {
+        _Collider.radius = mRadius;
+        Gizmos.color = mColorToGizmos;
+        Gizmos.DrawWireSphere(transform.position, mRadius);
+
+        Gizmos.color = Color.red;
+        for(int i = 0; i < mWifiPoints.Count; i++)
+        {
+            Gizmos.DrawLine(transform.position, mWifiPoints[i].transform.position);
+        }
+    }
 }
